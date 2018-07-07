444ba6e5b47486180e1d80795d8cc973477515c2
いちもつ青田 <ichimots@gmail.com>
2018-07-07 18:08:14 +0900

    init

---
+++ RegularExpression/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RegularExpression
+//
+//  Created by Akasaaa on 2018/07/06.
+//  Copyright © 2018年 Akasaaa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RegularExpression/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RegularExpression
+//
+//  Created by Akasaaa on 2018/07/06.
+//  Copyright © 2018年 Akasaaa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
